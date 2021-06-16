4a5f6ce8eb65e5414a75fec262da2439bb3c2468
Christopher Pepper <p3p@p3psoft.co.uk>
2021-06-16 19:23:09 +0100

    fix: restrict fileopen dialog docking

--- src/MarlinSimulator/user_interface.h
+++ src/MarlinSimulator/user_interface.h
@@ -37,77 +37,96 @@ Size=400,400
 Collapsed=0
 
 [Window][Components]
-Pos=995,0
-Size=285,720
+Pos=1003,0
+Size=277,720
 Collapsed=0
 DockId=0x00000006,0
 
 [Window][Simulation]
 Pos=0,0
-Size=270,159
+Size=313,107
 Collapsed=0
 DockId=0x00000003,0
 
 [Window][Viewport]
-Pos=272,0
-Size=721,386
+Pos=315,0
+Size=686,482
 Collapsed=0
 DockId=0x00000009,0
 
 [Window][Status]
-Pos=0,161
-Size=270,559
+Pos=0,109
+Size=313,611
 Collapsed=0
 DockId=0x00000004,0
 
 [Window][Signal Analyser]
-Pos=272,0
-Size=721,386
+Pos=315,0
+Size=686,482
 Collapsed=0
 DockId=0x00000009,1
 
 [Window][Pin List]
-Pos=995,0
-Size=285,720
+Pos=1003,0
+Size=277,720
 Collapsed=0
 DockId=0x00000006,1
 
 [Window][Serial Monitor(0)]
-Pos=272,388
-Size=721,332
+Pos=315,484
+Size=686,236
 Collapsed=0
 DockId=0x0000000A,0
 
 [Window][Serial Monitor(1)]
-Pos=272,388
-Size=721,332
+Pos=315,484
+Size=686,236
 Collapsed=0
 DockId=0x0000000A,1
 
 [Window][Serial Monitor(2)]
-Pos=272,388
-Size=721,332
+Pos=315,484
+Size=686,236
 Collapsed=0
 DockId=0x0000000A,2
 
 [Window][Serial Monitor(3)]
-Pos=272,388
-Size=721,332
+Pos=315,484
+Size=686,236
 Collapsed=0
 DockId=0x0000000A,3
 
+[Window][Serial Monitor]
+Pos=272,453
+Size=623,267
+Collapsed=0
+DockId=0x00000008,0
+
+[Window][Choose File##ChooseFileDlgKey]
+Pos=271,95
+Size=678,431
+Collapsed=0
+
+[Table][0x5E7B4F09,4]
+RefScale=13
+Column 0  Sort=0v
+Column 1
+Column 2
+Column 3
+
 [Docking][Data]
 DockSpace         ID=0x6F13380E Window=0x49B6D357 Pos=0,0 Size=1280,720 Split=X
-  DockNode        ID=0x00000005 Parent=0x6F13380E SizeRef=1389,720 Split=X
-    DockNode      ID=0x00000001 Parent=0x00000005 SizeRef=378,720 Split=Y Selected=0x7CAC602A
-      DockNode    ID=0x00000003 Parent=0x00000001 SizeRef=637,159 Selected=0x848745AB
-      DockNode    ID=0x00000004 Parent=0x00000001 SizeRef=637,559 Selected=0x7CAC602A
-    DockNode      ID=0x00000002 Parent=0x00000005 SizeRef=1009,720 Split=Y Selected=0x995B0CF8
+  DockNode        ID=0x00000005 Parent=0x6F13380E SizeRef=1001,720 Split=X
+    DockNode      ID=0x00000001 Parent=0x00000005 SizeRef=313,720 Split=Y Selected=0x7CAC602A
+      DockNode    ID=0x00000003 Parent=0x00000001 SizeRef=637,107 Selected=0x848745AB
+      DockNode    ID=0x00000004 Parent=0x00000001 SizeRef=637,611 Selected=0x7CAC602A
+    DockNode      ID=0x00000002 Parent=0x00000005 SizeRef=686,720 Split=Y Selected=0x995B0CF8
       DockNode    ID=0x00000007 Parent=0x00000002 SizeRef=448,451 Split=Y Selected=0x995B0CF8
-        DockNode  ID=0x00000009 Parent=0x00000007 SizeRef=623,386 CentralNode=1 Selected=0x995B0CF8
-        DockNode  ID=0x0000000A Parent=0x00000007 SizeRef=623,332 Selected=0x9E3BE472
+        DockNode  ID=0x00000009 Parent=0x00000007 SizeRef=623,482 CentralNode=1 Selected=0x995B0CF8
+        DockNode  ID=0x0000000A Parent=0x00000007 SizeRef=623,236 Selected=0xB516B7B1
       DockNode    ID=0x00000008 Parent=0x00000002 SizeRef=448,267 Selected=0xB42549D5
-  DockNode        ID=0x00000006 Parent=0x6F13380E SizeRef=399,720 Selected=0x18ABEC8D
+  DockNode        ID=0x00000006 Parent=0x6F13380E SizeRef=277,720 Selected=0xA115F62D
+
 
 )";
 
@@ -264,7 +283,7 @@ struct SerialMonitor : public UiWindow {
     if (ImGui::BeginMenuBar()) {
       if (ImGui::BeginMenu("Stream")) {
         if (ImGui::MenuItem("Select GCode File")) {
-          ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey", "Choose File", ".gcode,.gc,*", ".");
+          ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey", "Choose File", "GCode(*.gcode *.gc *.g){.gcode,.gc,.g},.*", ".");
         }
         if (input_file.is_open() && streaming)
           if (ImGui::MenuItem("Pause")) {
@@ -290,7 +309,7 @@ struct SerialMonitor : public UiWindow {
       ImGui::EndMenuBar();
     }
 
-    if (ImGuiFileDialog::Instance()->Display("ChooseFileDlgKey"))  {
+    if (ImGuiFileDialog::Instance()->Display("ChooseFileDlgKey", ImGuiWindowFlags_NoDocking))  {
       if (ImGuiFileDialog::Instance()->IsOk()) {
         std::string filePathName = ImGuiFileDialog::Instance()->GetFilePathName();
         printf("Streaming file: %s\n", filePathName.c_str());
